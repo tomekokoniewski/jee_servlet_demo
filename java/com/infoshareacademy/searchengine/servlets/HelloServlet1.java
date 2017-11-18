5740d9a888b1d2b5710f39121f8a02f7dd63a1b6
tomekokoniewski <alai@wp.pl>
2017-11-18 18:19:17 +0100

    asf

--- java/com/infoshareacademy/searchengine/servlets/HelloServlet1.java
+++ java/com/infoshareacademy/searchengine/servlets/HelloServlet1.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/hello-servlet1")
+public class HelloServlet1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         PrintWriter writer = resp.getWriter();
-        writer.println("Hello World!");
+        writer.println("Hello World! ");
+        writer.println("Hello World! Tomek");
+
     }
 }
