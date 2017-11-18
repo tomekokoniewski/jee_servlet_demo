e4855cae08ea8cd27dc5358f1c0a1c48999eb6a3
tomekokoniewski <alai@wp.pl>
2017-11-18 19:55:19 +0100

    20171118 1955

---
+++ src/main/java/com/infoshareacademy/searchengine/servlets/FindUserByIdServle.java
@@ -0,0 +1,45 @@
+package com.infoshareacademy.searchengine.servlets;
+
+import com.infoshareacademy.searchengine.dao.UsersRepositoryDao;
+import com.infoshareacademy.searchengine.dao.UsersRepositoryDaoBean;
+import com.infoshareacademy.searchengine.domain.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+@WebServlet("/find-user-by-id")
+public class FindUserByIdServle extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        if (req.getParameter("id") == null) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        int id = Integer.parseInt(req.getParameter("id"));
+
+        UsersRepositoryDao usersRepositoryDao = new UsersRepositoryDaoBean(); //ważne z jednej str Dao, z drugiej Bean, muszę DAO jako interface - JEE pracuje na interfeace
+        User user = usersRepositoryDao.getUserById(id);
+
+        if (user == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            PrintWriter printWriter = resp.getWriter();
+            printWriter.println("uzytk nie znaleziony");
+            return;
+        }
+
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println(user.getName());
+    }
+}
+
+//http://localhost:8080/find-user-by-id?id=1
+//http://localhost:8080/find-user-by-id?id=100 --uzytk nie znaleziony
---
+++ src/main/java/com/infoshareacademy/searchengine/servlets/WelcomeUserServlet.java
@@ -0,0 +1,41 @@
+package com.infoshareacademy.searchengine.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/welcome-user")
+public class WelcomeUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    if (req.getParameter("name")==null){
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+    }
+
+
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<!DOCTYPE html>");
+writer.println("<html>");
+        writer.println("<body>");
+
+writer.println("Hello "+req.getParameter("name")+"</br>");
+        writer.println("</body>");
+        writer.println("</html>");
+
+    }
+}
+
+
+//http://localhost:8080/welcome-user?name=Tomek
+//http://localhost:8080/welcome-user --pusta str --f12, network, odświeżyć str - ststus powiniem byc 400 (bad req)
+
